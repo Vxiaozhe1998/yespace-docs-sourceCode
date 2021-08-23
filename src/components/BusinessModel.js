1353811e04fea82d4b9f3106b58969c8443b6730
Vxiaozhe1998 <957798624@qq.com>
2021-08-23 22:55:06 +0800

    1.更新合作模式的样式 2.调整了合作模式移动端样式

--- src/components/BusinessModel.js
+++ src/components/BusinessModel.js
@@ -34,36 +34,24 @@ export default class BusinessModel extends React.Component {
   render() {
     return (
       <div className={clsx(styles.BusinessModel)}>
-        <div className="container">
-          <div className={clsx("row")}>
-            <div className="col col--8">
-              <h1 className={clsx(styles.headline)}>合作方案</h1>
-              <div className={clsx("row")}>
-                <div className="col col--7">
-                  <p className={clsx(styles.subtitle)}>
-                    悠悦<strong>免费提供</strong>
-                  </p>
-                  {itemList.map((props, idx) => (
-                    <Item key={idx} {...props} />
-                  ))}
-                  <div className={clsx(styles.charge)}>
-                    <p>&nbsp;仅对隔夜使用行为收取费用&nbsp;</p>
-                  </div>
-                </div>
-                <div className={clsx("col col--5"),styles.subFlex}>
-                  <p className={clsx(styles.subtitle)}>馆方提供</p>
-                  <div className={clsx(styles.tagSub)}>一般照明电源</div>
-                  <div className={clsx(styles.tagSub)}>产品部署场地</div>
-                </div>
-              </div>
-            </div>
-            <div className={clsx("col col--4")}>
-              <img
-                src="../../static/img/index/project.png"
-                className={clsx(styles.image)}
-              />
-            </div>
+        <div className={clsx("container", styles.mainCard)}>
+        <h1 className={clsx(styles.headline)}>合作方案</h1>
+          <div className={clsx(styles.yespaceCard)}>
+            <p className={clsx(styles.subtitle)}>
+              悠悦<strong>免费提供</strong>
+            </p>
+            {itemList.map((props, idx) => (
+              <Item key={idx} {...props} />
+            ))}
           </div>
+          <div className={clsx(styles.libraryCard)}>
+            <p className={clsx(styles.subtitle)}>馆方仅需提供</p>
+            <p className={clsx(styles.tagSub)}>一般照明电源</p>
+            <p className={clsx(styles.tagSub)}>产品部署场地</p>
+          </div>
+          <div className={clsx(styles.charge)}>
+          <p>&nbsp;仅对隔夜使用行为收取费用&nbsp;</p>
+        </div>
         </div>
       </div>
     );
